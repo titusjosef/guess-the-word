fe51db5b2d0514d89b74c670ba380c380fd447db
Titus Yick <tjyick@gmail.com>
2022-01-24 18:09:30 -0500

    I made all the letters that are typed in the console appear in an array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const guessedLetters = document.querySelector(".guessed-letters");
-const button = document.querySelector(".guess");
+const lettersToGuess = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
 const textInput = document.querySelector(".letter");
 const emptyParagraph = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
@@ -8,7 +8,8 @@ const emptyParagraphMessages = document.querySelector(".message");
 
 const hiddenButton = document.querySelector(".play-again");
 
-const word = "magnolia"
+const word = "magnolia";
+const guessedLetters = [];
 
 const paragraphSymbols = function (word) {
     const dotLetterHolders = [];
@@ -20,15 +21,37 @@ const paragraphSymbols = function (word) {
 };
 paragraphSymbols(word);
 
-button.addEventListener("click", function (e) {
+guessButton.addEventListener("click", function (e) {
 
     e.preventDefault();
-    const inputValue = textInput.value; 
-    console.log(inputValue);
-    textInput.value="";
-
-
+    emptyParagraphMessages.innerText = "";
+    const inputValue = textInput.value;
+    const goodGuess =  acceptInput(inputValue);
+    if (goodGuess) {
+        makeGuess(inputValue);
+    }
+    textInput.value = "";
+    const inputCheck = acceptInput(inputValue);
 });
 
+const acceptInput =  function (input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
 
-
+    emptyParagraphMessages.innerText = "Enter a letter"
+    } else if (input.length > 1) {
+    emptyParagraphMessages.innerText = "Enter only one letter."; 
+    } else if (!input.match(acceptedLetter)) {
+        emptyParagraphMessages.innerText = "Enter a letter from A-Z.";
+    } else {
+        return input; 
+    }
+};
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if(guessedLetters.includes(guess)) {
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    } 
+};
